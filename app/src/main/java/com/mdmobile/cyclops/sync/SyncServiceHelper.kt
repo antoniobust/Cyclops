3cf4c19d86793da2dfd052a4ab6dbb5c11384607
Antonio <antoniobust@gmail.com>
2018-07-10 17:34:54 +0200

    Add licencing library untracked files

---
+++ app/src/main/java/com/mdmobile/cyclops/sync/SyncServiceHelper.kt
@@ -0,0 +1,37 @@
+package com.mdmobile.cyclops.sync
+
+import android.content.Context
+import android.util.Log
+import com.mdmobile.cyclops.apiManager.ApiRequestManager
+import com.mdmobile.cyclops.dataModel.Server
+import com.mdmobile.cyclops.sync.SyncService.*
+import com.mdmobile.cyclops.utils.Logger
+
+class SyncServiceHelper {
+    companion object {
+        private val LOG_TAG = SyncServiceHelper::class.java.simpleName
+        fun sync(context: Context, serverList: List<Server>, actions: ArrayList<String>) {
+
+            Logger.log(LOG_TAG, "Syncing servers...\nAction to perform:" + actions.toString(), Log.VERBOSE)
+//            val intent = Intent(MainActivity.UPDATE_LOADING_BAR_ACTION)
+//            intent.putExtra(MainActivity.UPDATE_LOADING_BAR_ACTION_COUNT, actions.size)
+//            intent.setPackage(getContext().getPackageName())
+            for (server in serverList) {
+                Logger.log(LOG_TAG, "Syncing ${server.serverName}...", Log.VERBOSE)
+                for (action in actions) {
+                    when (action) {
+                        SYNC_DEVICES ->
+                            //context.sendBroadcast(intent)
+                            ApiRequestManager.getInstance().getDeviceInfo(server)
+                        SYNC_SERVER ->
+                            //context.sendBroadcast(intent)
+                            ApiRequestManager.getInstance().getServicesInfo(server)
+                        SYNC_USERS ->
+                            //context.sendBroadcast(intent)
+                            ApiRequestManager.getInstance().getUsers(server)
+                    }
+                }
+            }
+        }
+    }
+}

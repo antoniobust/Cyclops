7dc072a89bc5d30ec6dc375e0ce9e9699713b3af
Antonio <antoniobust@gmail.com>
2018-10-04 21:06:40 +0100

    Add missing slash to APIs

--- app/src/main/java/com/mdmobile/cyclops/api/McApiService.kt
+++ app/src/main/java/com/mdmobile/cyclops/api/McApiService.kt
@@ -13,44 +13,43 @@ import retrofit2.http.*
 interface McApiService {
 
     //Devices APIs
-    @GET("devices")
+    @GET("/devices")
     fun getDevices(): Call<List<BasicDevice>>
 
-    @GET("search")
+    @GET("/search")
     fun getDevicesV14(): Call<List<BasicDevice>>
 
-    @GET("devices/{deviceId}")
+    @GET("/devices/{deviceId}")
     fun getDevice(@Path("deviceId") devId: String): Call<BasicDevice>
 
-    @DELETE("devices/{deviceId}")
+    @DELETE("/devices/{deviceId}")
     fun deleteDevice(): Call<Void>
 
-    @POST("devices/{deviceId}/actions")
+    @POST("/devices/{deviceId}/actions")
     fun sendAction(@Path("deviceId") devId: String, @Body action: Action): Call<Void>
 
-    @GET("api/devices/{deviceId}/collectedData")
+    @GET("/api/devices/{deviceId}/collectedData")
     fun getCollectedData(@Path("deviceId") devId: String): Call<List<CollectedData>>
 
-    @GET("api/devices/collectedData")
+    @GET("/api/devices/collectedData")
     fun getAllCollectedData(): Call<List<CollectedData>>
 
-    @GET("api/devices/{deviceId}/installedApplications")
+    @GET("/api/devices/{deviceId}/installedApplications")
     fun getInstalledApps(@Path("deviceId") devId: String): Call<List<InstalledApp>>
 
-    @POST("api/devices/{deviceId}/profiles/{profileId}/actions")
+    @POST("/api/devices/{deviceId}/profiles/{profileId}/actions")
     fun getDeviceProfiles(@Path("deviceId") devId: String, @Path("profileId") profileId: String): Call<Void>
 
-    @POST("api/devices/{deviceId}/profiles")
+    @POST("/api/devices/{deviceId}/profiles")
     fun getDeviceProfiles(@Path("deviceId") devId: String): Call<List<Profile>>
 
 
     //Server APIs
-    @GET("api/servers")
+    @GET("/api/servers")
     fun getServers(): Call<List<ServerInfo>>
 
     //User APIs
-    @GET("api/security/users")
+    @GET("/api/security/users")
     fun getInstalledApps(): Call<List<User>>
-
 
 }

6e10d874e57c80621d29095e70c6b7487a03b551
Antonio <antoniobust@gmail.com>
2018-10-04 21:26:38 +0100

    Add missing API parameters

--- app/src/main/java/com/mdmobile/cyclops/api/McApiService.kt
+++ app/src/main/java/com/mdmobile/cyclops/api/McApiService.kt
@@ -2,6 +2,8 @@ package com.mdmobile.cyclops.api
 
 import com.mdmobile.cyclops.dataModel.api.*
 import com.mdmobile.cyclops.dataModel.api.devices.BasicDevice
+import com.mdmobile.cyclops.dataTypes.ComplexDataType
+import com.mdmobile.cyclops.dataTypes.ProfileActions
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -14,7 +16,14 @@ interface McApiService {
 
     //Devices APIs
     @GET("/devices")
-    fun getDevices(): Call<List<BasicDevice>>
+    fun getDevices(
+            @Query("path") path: String?,
+            @Query("skip") skip: Int?,
+            @Query("take") take: Int?,
+            @Query("order") order: String?,
+            @Query("filter") filter: String?,
+            @Query("userFilter") userFilter: String?
+    ): Call<List<BasicDevice>>
 
     @GET("/search")
     fun getDevicesV14(): Call<List<BasicDevice>>
@@ -29,19 +38,35 @@ interface McApiService {
     fun sendAction(@Path("deviceId") devId: String, @Body action: Action): Call<Void>
 
     @GET("/api/devices/{deviceId}/collectedData")
-    fun getCollectedData(@Path("deviceId") devId: String): Call<List<CollectedData>>
+    fun getCollectedData(
+            @Path("deviceId") devId: String,
+            @Query("startDate") startData: String,
+            @Query("endDate") endDate: String,
+            @Query("builtInDataType") builtInDataType: ComplexDataType.BuiltInDataType?,
+            @Query("customDataType") customDataType: String?
+    ): Call<List<CollectedData>>
 
     @GET("/api/devices/collectedData")
-    fun getAllCollectedData(): Call<List<CollectedData>>
+    fun getAllCollectedData(
+            @Query("startDate") startData: String,
+            @Query("endDate") endDate: String,
+            @Query("builtInDataType") builtInDataType: ComplexDataType.BuiltInDataType?,
+            @Query("customDataType") customDataType: String?,
+            @Query("path") path: String?,
+            @Query("skip") skip: Int?,
+            @Query("take") take: Int?
+    ): Call<List<CollectedData>>
 
     @GET("/api/devices/{deviceId}/installedApplications")
     fun getInstalledApps(@Path("deviceId") devId: String): Call<List<InstalledApp>>
 
-    @POST("/api/devices/{deviceId}/profiles/{profileId}/actions")
-    fun getDeviceProfiles(@Path("deviceId") devId: String, @Path("profileId") profileId: String): Call<Void>
-
-    @POST("/api/devices/{deviceId}/profiles")
+    @GET("/api/devices/{deviceId}/profiles")
     fun getDeviceProfiles(@Path("deviceId") devId: String): Call<List<Profile>>
+
+    @POST("/api/devices/{deviceId}/profiles/{profileId}/actions")
+    fun deviceProfileAction(@Path("deviceId") devId: String,
+                            @Path("profileId") profileId: String,
+                            @Body @ProfileActions action: String): Call<Void>
 
 
     //Server APIs

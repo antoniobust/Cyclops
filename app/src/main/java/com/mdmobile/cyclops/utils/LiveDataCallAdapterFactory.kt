50a3428c889192ae60913cb55f71fb3186f05d86
Antonio <antoniobust@gmail.com>
2018-10-07 21:58:02 +0100

    Add dagger dependency

--- app/src/main/java/com/mdmobile/cyclops/utils/LiveDataCallAdapterFactory.kt
+++ app/src/main/java/com/mdmobile/cyclops/utils/LiveDataCallAdapterFactory.kt
@@ -1,4 +1,11 @@
 package com.mdmobile.cyclops.utils
 
-class LiveDataCallAdapterFactory {
+import retrofit2.CallAdapter
+import retrofit2.Retrofit
+import java.lang.reflect.Type
+
+class LiveDataCallAdapterFactory: CallAdapter.Factory() {
+    override fun get(returnType: Type, annotations: Array<Annotation>, retrofit: Retrofit): CallAdapter<*, *>? {
+
+    }
 }

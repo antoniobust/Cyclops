0bc14aeac8536c8a65e05baed67fb7641f634502
Antonio <antoniobust@gmail.com>
2018-12-29 09:32:33 +0000

    Add mockito dependencies

---
+++ app/src/test/java/com/mdmobile/cyclops/api/DeviceRepositoryTest.kt
@@ -0,0 +1,17 @@
+package com.mdmobile.cyclops.api
+
+import android.app.Instrumentation
+import com.mdmobile.cyclops.db.MobiControlDB
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+
+class DeviceRepositoryTest {
+
+
+    @Before
+    fun init(){
+        val db = mock
+    }
+}

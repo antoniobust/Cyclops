7e82da4471c05afd5b6cd1dcf4e036860403cbd3
Antonio <antoniobust@gmail.com>
2019-01-07 22:23:54 +0000

    Ignore id in instanceInfo pojo

--- app/src/main/java/com/mdmobile/cyclops/dataModel/api/newDataClass/InstanceInfo.kt
+++ app/src/main/java/com/mdmobile/cyclops/dataModel/api/newDataClass/InstanceInfo.kt
@@ -1,6 +1,7 @@
 package com.mdmobile.cyclops.dataModel.api.newDataClass
 
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import com.mdmobile.cyclops.provider.McContract
 
@@ -10,8 +11,20 @@ data class InstanceInfo(
         val id: Int,
         val serverName: String = "N/A",
         val apiSecret: String = "N/A",
-        val clientId: String= "N/A",
-        val serverAddress: String= "N/A",
+        val clientId: String = "N/A",
+        val serverAddress: String = "N/A",
         val serverMajorVersion: Int = -1,
         val buildNumber: Int = -1,
-        val currentToken: String = "N/A")
+        val currentToken: String = "N/A") {
+
+    @Ignore
+    constructor(
+            serverName: String = "N/A",
+            apiSecret: String = "N/A",
+            clientId: String = "N/A",
+            serverAddress: String = "N/A",
+            serverMajorVersion: Int = -1,
+            buildNumber: Int = -1,
+            currentToken: String = "N/A"
+    ) : this(-1, serverName, apiSecret, clientId, serverAddress, serverMajorVersion, buildNumber, currentToken)
+}
